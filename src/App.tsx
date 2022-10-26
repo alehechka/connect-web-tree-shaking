55732bac5fc707e2acb17cb10bcfca9007c07956
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-10-26 14:16:34 -0500

    proto definitions and generated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { createPromiseClient, createConnectTransport } from '@bufbuild/connect-web';
+import { ExampleService } from './proto/gen/ts/example/v1/example_connectweb';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	useEffect(() => {
+		const ExampleClient = createPromiseClient(ExampleService, createConnectTransport({ baseUrl: '/' }));
+		ExampleClient.example({});
+	}, []);
+
+	return (
+		<div className='App'>
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src='/vite.svg' className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://reactjs.org' target='_blank'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
